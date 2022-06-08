0ad430607cc7ebe7ec1e510a9d9ffda45f9bf032
chuan <honganchen53@gmail.com>
2022-06-08 21:08:14 +0800

    22/6/8/21:08

--- Sprout/s399.cpp
+++ Sprout/s399.cpp
@@ -6,8 +6,29 @@
 
 using namespace std;
 
-int main(){
+#define int long long
+signed main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    
+    int n, ans = 0;
+    cin >> n;
+    vector<pair<int,int>> s(n);
+    for(int i=0; i<n; i++){
+        cin >> s[i].first >> s[i].second;
+    }
+    map<int,int> mp;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            int x = s[i].first-s[j].first;
+            int y = s[i].second-s[j].second;
+            int dis = x*x + y*y;
+            // cout << i << ' '<<j<<" " << dis<<"\n";
+            mp[dis]+=1;
+        }
+    }
+    for(auto i:mp){
+        if(i.second > 1)
+            ans += (i.second)*(i.second-1)/2;
+    }
+    cout << ans <<'\n';
 }
